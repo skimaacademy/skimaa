e00aaaca3ceda7a3a108da3c2721501023d256cf
Md. Arafat Hossain <arafathossainarafat6@gmail.com>
2024-10-23 23:59:18 +0600

    Update footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -7,24 +7,26 @@ const Footer: FC = () => {
   return (
     <footer className="">
       <div className='mx-4 md:mx-6'>
-        <div className="py-6 mt-5 border-t container mx-auto flex flex-col md:flex-row justify-between items-center">
-          <div className="text-center md:text-left">
+        <div className="py-6 mt-5 border-t container mx-auto flex flex-col justify-start lg:flex-row lg:justify-between lg:items-center md:gap-2 lg:gap-2">
+          <div className="md:text-left">
             <SkimaaFullLogo />
             <p className="text-sm mt-1">
               Empowering your future through education.
             </p>
           </div>
+
           <div className="mt-4 md:mt-0">
-            <nav className="flex flex-col md:flex-row space-x-5">
+            <nav className="flex flex-col sm:flex-row sm:gap-5">
               {
                 NavLinksWithName.map((nav, index) => (
-                  <a key={index} href={nav.href} className="hover:text-gray-400">
+                  <a key={index} href={nav.href} className="hover:text-secondary-foreground/60">
                     {nav.name}
                   </a>
                 ))
               }
             </nav>
           </div>
+
           <div className="mt-4 md:mt-0">
             <p className="text-sm">© {new Date().getFullYear()} Skimaa. All rights reserved.</p>
           </div>
